a84c0ca27f35dc4b2bfae9196758d55a7bd44bbd
Rebecca Meritz <rebecca@meritz.com>
2013-05-11 10:56:40 +0200

    We have a logged in user

--- critical_critiques/critical_critiques/settings/base.py
+++ critical_critiques/critical_critiques/settings/base.py
@@ -234,7 +234,12 @@ LOGGING = {
             'level': 'DEBUG',
             'propagate': True,
         },
-    }
+        'critical_critiques': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
 }
 ########## END LOGGING CONFIGURATION
 
@@ -259,3 +264,5 @@ GITHUB_APP_ID = '86a483896d08237f9062'
 GITHUB_API_SECRET = '8bc213d537311efb600445b667c7e294c5e8ffb9'
 
 LOGIN_REDIRECT_URL = '/signin/'
+LOGIN_REDIRECT_URL = '/signin/done/'
+LOGIN_ERROR_URL    = '/signin/done/'
--- critical_critiques/signin/views.py
+++ critical_critiques/signin/views.py
@@ -3,7 +3,9 @@ from django.template import RequestContext
 from django.shortcuts import render_to_response
 from social_auth.db.django_models import UserSocialAuth
 from github3.api import login
+import logging
 
+logger = logging.getLogger(__name__)
 
 def home(request):
     if request.user.is_authenticated():
@@ -13,10 +15,11 @@ def home(request):
 
 
 def done(request):
-    """List the gists"""
-    user = request.user
-    social_auth = UserSocialAuth.objects.get(user=user)
-    tokens = social_auth.tokens
-    github = login(token=tokens['access_token'])
-    gists = [g for g in github.iter_gists()]
+    # user = request.user
+    # logger.debug("THING")
+    # logger.debug(user)
+    # social_auth = UserSocialAuth.objects.get(user=user.pk)
+    # tokens = social_auth.tokens
+    # github = login(token=tokens['access_token'])
+    # gists = [g for g in github.iter_gists()]
     return render_to_response('done.html', RequestContext(request))
