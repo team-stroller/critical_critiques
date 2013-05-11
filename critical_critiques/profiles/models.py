af6f68e22f56646f061e3fa853939307344ed0dc
Anton Agestam <msn@antonagestam.se>
2013-05-11 11:57:31 +0200

    added profiles to installed apps

--- critical_critiques/profiles/models.py
+++ critical_critiques/profiles/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, primary_key=True)
+    user = models.OneToOneField(User, primary_key=True, related_name='profile')
     due_reviews = models.IntegerField(u'Due reviews', default=2,
                                       help_text=u'Number of reviews user can '
                                                 u'create')
