5171a9256bb8161db02fde8f5c07da5ac5c81abf
Rebecca Meritz <rebecca@meritz.com>
2013-05-11 16:22:19 +0200

    Basic submission form validation

--- critical_critiques/submission/forms.py
+++ critical_critiques/submission/forms.py
@@ -1,10 +1,19 @@
-from django.forms import ModelForm
+from django import forms
 
 from .models import Submission
 
 
-class SubmissionForm(ModelForm):
+class SubmissionForm(forms.ModelForm):
 
     class Meta:
         model = Submission
         exclude = ('user',)
+
+    def clean_url(self):
+        url = self.cleaned_data['url']
+        # Pull Request: https://github.com/basho/webmachine/pull/143
+        # Gist: https://gist.github.com/rmeritz/2863145
+        if "github.com" not in url:
+            raise forms.ValidationError(
+                "Must be a URl for a github pull request")
+        return url
