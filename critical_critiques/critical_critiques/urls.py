9c3c1f539c0fcb140422d8c67525cba01ad5dc1d
Anton Agestam <msn@antonagestam.se>
2013-05-11 10:01:22 +0200

    serve static files

--- critical_critiques/critical_critiques/urls.py
+++ critical_critiques/critical_critiques/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.conf.urls.static import static
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +20,5 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
